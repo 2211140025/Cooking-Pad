0005a1c0d947394ca9c85935c77c38312a834045
Katsuno Sho <2211140025x@ed.fuk.kindai.ac.jp>
2024-11-13 08:00:37 +0000

    top_controller

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,10 +1,24 @@
 class TopController < ApplicationController
   def main
+    if session[:login_uid]  
+        ##redirect_to
+    else
+        render "login"
+    end
   end
   
   def login
+    user = User.find_by(user_id: params[:user_id])
+    if user and BCrypt::Password.new(user.pass_digest) == params[:user_pass]
+        session[:login_uid] = params[:user_id]
+        redirect_to top_main_path
+    else
+        render "error", status: 422
+    end
   end
   
   def logout
+    session.delete(:login_uid)
+    redirect_to root_path
   end
 end
