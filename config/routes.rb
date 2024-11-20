4e1dc07004118a51c7698728730e73b2eee5d3d5
kameyamakakeru <2211140012n@ed.fuk.kindai.ac.jp>
2024-11-20 08:01:14 +0000

    Update routes.rb with [変更内容の説明]

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'top/logout'
   post 'top/login_form'
   get 'top/main'
-  resource :users
+  resources :sessions, only: [:new, :create, :destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
