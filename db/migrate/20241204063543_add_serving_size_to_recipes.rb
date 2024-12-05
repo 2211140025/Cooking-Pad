3e06877deb3c4e6f860e95040890b28631b4d456
kameyamakakeru <2211140012n@ed.fuk.kindai.ac.jp>
2024-12-05 08:07:02 +0000

    Kakeru sinki syusei data karanisita

--- db/migrate/20241204063543_add_serving_size_to_recipes.rb
+++
@@ -1,5 +0,0 @@
-class AddServingSizeToRecipes < ActiveRecord::Migration[6.1]
-  def change
-    add_column :recipes, :serving_size, :integer
-  end
-end
--- db/migrate/20241204064033_add_tips_to_recipes.rb
+++
@@ -1,5 +0,0 @@
-class AddTipsToRecipes < ActiveRecord::Migration[7.1]
-  def change
-    add_column :recipes, :tips, :text
-  end
-end
